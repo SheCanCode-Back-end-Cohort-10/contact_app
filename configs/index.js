9de5ff93bcdaeb0a066397aba7da61e219c24f55
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-03-27 13:12:28 +0200

    Update: Some changes in the index.js file.

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,7 @@
-export default configs = {
+const configs = {
     port: process.env.PORT || 3000,
     mongoURI: process.env.MONGODB_URI ||'mongodb://localhost:27017/contacts',
     secret: process.env.SECRET ||'mysecret'
 }
+
+export default configs;
