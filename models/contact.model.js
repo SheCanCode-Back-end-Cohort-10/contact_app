74d32d77c09cdce826275a5ab464badc8464ef23
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-03-27 12:41:10 +0200

    model commit

--- models/contact.model.js
+++ models/contact.model.js
@@ -1,1 +1,27 @@
 import mongoose from 'mongoose';
+
+const contactModel=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "you must provide the name  "]
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+export default contactModel
