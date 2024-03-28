53c57513f2458b7be7d2ebdcc84170da8b47fd98
Alice-babayalo <umuhozawasealice@gmail.com>
2024-03-28 12:23:11 +0200

    review

--- configs/index.js
+++ configs/index.js
@@ -1,5 +1,5 @@
 const configs = {
-    port: process.env.PORT||5000,
+    port: process.env.PORT||2000,
     mongoURI: process.env.MONGODB_URI||"mongodb+srv://Alice:12345@cluster0.6eqmb7j.mongodb.net/contacts",
     secret: process.env.SECRET
 }
