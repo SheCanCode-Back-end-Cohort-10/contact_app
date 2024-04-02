ff4c456b7e00085385106f548ecdc667e463c868
Aline-CROIRE <niyocroirealine@gmail.com>
2024-04-02 02:48:34 -0700

    renamed index.js to app.js and modified other files

--- configs/index.js
+++ configs/index.js
@@ -1,6 +1,6 @@
 const configs = {
     port: process.env.PORT || 3000,
-    mongoURI: process.env.MONGODB_URI ||'mongodb://localhost:27017/contacts',
+    mongoURI: process.env.MONGODB_URI || 'mongodb+srv://niyocroirealine:WrQb8AXOcGA4eNrf@cluster0.ebwlnjc.mongodb.net/ContactApp',
     secret: process.env.SECRET ||'mysecret'
 }
 
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,1 +1,21 @@
 // Contact routes here
+
+import express from 'express';
+import contactController from '../controllers/contact.controller.js';
+const router=express.Router();
+
+//create account route
+router.route('/create').post(contactController.createContact);
+router.route('/list').get(contactController.getAllContact);
+router.route('/list/:id').get(contactController.getContactById);
+router.route('/email/:email').get(contactController.getContactByEmail);
+router.route('/phone/:phone').get(contactController.getContactByPhone);
+router.route('/firstname/:firstName').get(contactController.getContactByFirstname);
+router.route('/lastname/:lastName').get(contactController.getContactByLastname);
+router.route('/list/:id').delete(contactController.deleteContact);
+router.route('/update/:id').put(contactController.updateContact);
+
+export default router
+
+
+
