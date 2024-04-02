cc9f101a0a48fd5142ab996db2cc2d8ea248321a
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-04-02 11:43:37 +0200

    contact project

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ import configuration from './configs/index.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import router from './routes/contact.routes.js';
+import notFound from './middlewares/notFound.js';
+import errorHandlerMiddleware from './middlewares/erroehandling.js';
 
 app.use(express.json());
 app.use(cors());
 app.use(router)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 mongoose.connect(configuration.mongoURI)
 .then(() => {
