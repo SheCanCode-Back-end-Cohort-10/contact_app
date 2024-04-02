ffecde2e627503fcab96378ee4537106e710c949
ImpuhweStella <stellahimpuhwe346@gmail.com>
2024-04-02 10:38:45 +0200

    changes in routes file

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,1 +1,10 @@
-// Contact routes here
+// Contact routes hereconst express = require('express');
+const router = express.Router();
+const contactController = require('../controllers/contactController');
+
+router.post('/createContact', contactController.createContact);
+router.put('/updateContact/:id', contactController.updateContact);
+router.delete('/deleteContact/:id', contactController.deleteContact);
+router.get('/:id', contactController.findContactById);
+
+module.exports = router;
